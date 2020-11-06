b814261ef69e8a141182526b3b8f3a1458f5a69e
jane_moody <janeemoody2@gmail.com>
2020-11-06 12:36:14 -0500

    Finished Task 5 now finishing the rest now then will go back to Task 4.

--- index.js
+++ index.js
@@ -288,9 +288,11 @@ Create a function called `removeArtist` that takes two arguments:
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset and log the number 19.  
 */
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(array, index) {
+  delete array[index];
+  return artists;
 }
+console.log(removeArtist(artists, 0));
 
 /**
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
